148fee44aa68d0920b64d4ec9ac13aa197f4bc61
Taelim Hwang <ghkdxofla@gmail.com>
2018-09-25 14:29:51 +0900

    add moving and rotating feature

--- tetris/CppTetris/CppTetris/main.cpp
+++ tetris/CppTetris/CppTetris/main.cpp
@@ -1,24 +1,118 @@
+/*
+* Author   : Taelim Hwang
+* Date     : 2018-09-11
+* Title    : Cpp Tetris
+* contents : This code is for making simple tetris game.
+*            Code is written in C++.
+*            Original source code is made by FamTrinli.
+*/
 #include <SFML/Graphics.hpp>
+#include <time.h>
+#include <cstdio>
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+const int M = 20;
+const int N = 10;
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+// Set field
+int field[M][N] = { 0 };
+
+// Define point
+// Point를 설정 함과 동시에 할당함
+struct Point {
+	int x, y;
+} a[4], b[4];
+
+// Set tetrimino
+int figures[7][4] = {
+	1, 3, 5, 7, // I
+	2, 4, 5, 7, // Z
+	3, 5, 4, 6, // S
+	3, 5, 4, 7, // T
+	2, 3, 5, 7, // L
+	3, 5, 7, 6, // J
+	2, 3, 4, 5, // O
+};
+
+int main(){
+	// Init game window
+	sf::RenderWindow window(sf::VideoMode(320, 480), "The Tetris");
+
+
+	// Set texture
+	sf::Texture t;
+	t.loadFromFile("image/tiles.png");
+
+
+	// Set sprite
+	sf::Sprite s(t);
+	s.setTextureRect(sf::IntRect(0, 0, 18, 18));
+
+	// Set feature of tetromino
+	int dx = 0;
+	bool rotate = 0;
+	int colorNum = 1;
+
+	// Window event codes
+	while (window.isOpen()) {
+		// Init event
+		sf::Event e;
+
+
+		// Game codes within event
+		while (window.pollEvent(e)) {
+			// Quit game
+			if (e.type == sf::Event::Closed)
 				window.close();
+
+			// Key pressed
+			if (e.type == sf::Event::KeyPressed)
+				if (e.key.code == sf::Keyboard::Up) rotate = true;
+				else if (e.key.code == sf::Keyboard::Left) dx = -1;
+				else if (e.key.code == sf::Keyboard::Right) dx = 1;
 		}
 
-		window.clear();
-		window.draw(shape);
+		// Move tetromino
+		for (int i = 0; i < 4; i++)
+			a[i].x += dx;
+
+		// Rotate tetromino
+		// 알고리즘 문제에서 회전하는 부분을 구할 때 요긴할듯
+		if (rotate) {
+			Point p = a[1];
+			for (int i = 0; i < 4; i++) {
+				int x = a[i].y - p.y;
+				int y = a[i].x - p.x;
+				a[i].x = p.x - x;
+				a[i].y = p.y + y;
+			}
+		}
+
+		// Make tetromino
+		int n = 3;
+		
+		// a의 값이 0으로 초기화된 경우 블록을 넣어주는 것
+		if (a[0].x == 0) {
+			for (int i = 0; i < 4; i++) {
+				a[i].x = figures[n][i] % 2;
+				a[i].y = figures[n][i] / 2;
+
+			}
+		}
+
+		// After block is moved...
+		dx = 0;
+		rotate = 0;
+
+		// Set window background
+		window.clear(sf::Color::White);
+
+		// Draw tetromino
+		for (int i = 0; i < 4; i++) {
+			s.setPosition(a[i].x * 18, a[i].y * 18);
+			window.draw(s);
+		}
+		window.draw(s);
 		window.display();
 	}
-
 	return 0;
 }
